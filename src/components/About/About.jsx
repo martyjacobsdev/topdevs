d4f5860d9e1e6ed259f806ed804e878c316b43ed
marty <martin.philip.jacobs@gmail.com>
2019-03-22 21:50:57 +1000

    test

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,22 +15,16 @@ class About extends Component {
         <Card className="md-grid md-cell--8">
           <div className="about-wrapper">
             <CardText>
-              <h2>A PWA demo with Gastby &amp; Snipcart</h2>
+              <h2>About us?</h2>
             </CardText>
-            <img
-              src={image}
-              className="about-img"
-              alt={config.siteTitle}
-            />
+           
             <CardText>
               <p className="about-text md-body-1">
-                This PWA e-commerce example was built using Gastby &amp; Snipcart.
-                You should try it on mobile! ;)
-              </p>
+                We just like the best stuff.
+                              </p>
               <p className="about-text md-body-1">
-                Oh, and the full tutorial is available {" "}
-                <a href="https://snipcart.com/blog/pwa-example-ecommerce-gatsby">here</a>.
-              </p>
+
+             </p>
 
             </CardText>
             <UserLinks labeled config={config} />
